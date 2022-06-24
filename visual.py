6ebf3d320408ea4ea5e8e84ca418f40085b92f76
tfederico <federicotavella.7@gmail.com>
2022-06-24 15:29:21 +0100

    add whole humanoid

--- visual.py
+++ visual.py
@@ -9,7 +9,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath()) #used by loadURDF
 p.configureDebugVisualizer(p.COV_ENABLE_GUI,0, rgbBackground=[255, 255, 255])
 
 
-id = p.loadURDF("/home/federico/Git/ASLMimic/data/humanoid/whole_upper_humanoid.urdf", basePosition=[0, 0, -0.5], baseOrientation=[ 0.7071068, 0, 0, 0.7071068 ], useFixedBase=True)
+id = p.loadURDF("/home/federico/Git/ASLMimic/data/humanoid/whole_upper_humanoid.urdf", basePosition=[0, 0,0], baseOrientation=[ 0.7071068, 0, 0, 0.7071068 ], useFixedBase=True, globalScaling=2)
 
 input("Press Enter to continue...")
 p.disconnect()
